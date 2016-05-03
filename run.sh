5a98587617a92350d1116c7d00afb670bcf7fb6e
dirodriguez <drodriguezm@udec.cl>
2016-05-03 17:39:28 -0300

    holi

--- run.sh
+++
@@ -1,4 +0,0 @@
-
-javac -d "tarea2/classes" tarea2/src/BufferedReaderExample.java
-javac -d "tarea2/classes" -classpath "tarea2/classes" tarea2/src/Tarea2.java
-java -classpath "tarea2/classes" tarea2.Tarea2
